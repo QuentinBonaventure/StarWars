3209c9ab0665fe22001339caf0897bf444cf2d37
Quentin Bona <bonaventurequentin@gmail.com>
2020-11-01 21:38:48 +0100

    Ajout interface combattre et instance de personne : citoyen. Suppression de methode combattre pour en faire une interface

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import personnages.Citoyen;
+import personnages.Personne;
+
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         Vaisseau vaisseauBase = new Vaisseau();
+        Personne.name ="xWing";
+        Citoyen citoyen = new Citoyen(Personne.name);
 
         vaisseauBase.setName();
 
@@ -12,6 +17,8 @@ public class Main {
         vaisseauBase.tirer();
         vaisseauBase.voler();
         vaisseauBase.atterir();
+
+        citoyen.afficheCamps();
 
 
 
--- src/personnages/Personne.java
+++ src/personnages/Personne.java
@@ -1,7 +1,8 @@
 package personnages;
 
 public abstract class Personne {
-    private final String name;
+    public static String name;
+
 
     public Personne(String name) {
         this.name = name;
@@ -11,5 +12,5 @@ public abstract class Personne {
         return name;
     }
     public abstract void afficheCamps();
-    public abstract void combattre();
+
 }
